6b9e3319bd1e97b9f11e58da1d7b42f6fc464bde
subado <redcatgamer1980@gmail.com>
2023-03-06 15:21:00 +0400

    Add custom ctags target

--- extra.py
+++ extra.py
@@ -1,4 +1,11 @@
 Import("env")
-
-# include toolchain paths
 env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+
+env.AddCustomTarget(
+    name="ctags",
+    dependencies=None,
+    actions=[
+        "ctags -R ${PROJECT_SRC_DIR} ${PROJECT_INCLUDE_DIR} ${PROJECT_LIBDEPS_DIR}"
+    ],
+    description="Generated tags file"
+)
