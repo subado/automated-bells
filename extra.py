d671fc8d17fc758d6e0511c169779e5b5b8c08a2
subado <redcatgamer1980@gmail.com>
2023-03-09 02:16:53 +0400

    Delete ctags because it is extra

--- extra.py
+++ extra.py
@@ -1,11 +1,4 @@
 Import("env")
-env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
 
-env.AddCustomTarget(
-    name="ctags",
-    dependencies=None,
-    actions=[
-        "ctags -R ${PROJECT_SRC_DIR} ${PROJECT_INCLUDE_DIR} ${PROJECT_LIBDEPS_DIR}"
-    ],
-    title="Generated tags file"
-)
+# include toolchain paths
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
