8bed1883092636f12118a9078e790557e576a9ea
subado <redcatgamer1980@gmail.com>
2023-03-08 19:10:21 +0400

    Add prettierrc and formatted all files

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from 'react'
+
+fetch('http://192.168.0.177/tables', {
+  method: 'GET',
+})
+  .then((req) => {
+    return req.json()
+  })
+  .then((data) => {
+    console.log(data)
+  })
 
 function App() {
-	return (
-		<div>
-		</div>
-	)
+  return <div className='container mx-auto pt-5 max-w-2xl'></div>
 }
 
-export default App;
+export default App
