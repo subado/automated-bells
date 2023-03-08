b6e8cb5611a4679c4ac8056074b8e6e5bbf2ddf4
subado <redcatgamer1980@gmail.com>
2023-03-08 19:11:22 +0400

    Delete extra fetch

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,5 @@
 import React from 'react'
 
-fetch('http://192.168.0.177/tables', {
-  method: 'GET',
-})
-  .then((req) => {
-    return req.json()
-  })
-  .then((data) => {
-    console.log(data)
-  })
-
 function App() {
   return <div className='container mx-auto pt-5 max-w-2xl'></div>
 }
