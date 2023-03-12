4952be4a049b202857c7c228f24dbff03e3db57d
subado <redcatgamer1980@gmail.com>
2023-03-13 00:28:18 +0400

    Add all components to App

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { sheduleAPI } from './api/sheduleAPI'
+import { Time } from './components/Time'
+import { Table } from './components/Table/Table'
+import { TableProvider } from './contexts/TableContext'
+import { TimeProvider } from './contexts/TimeContext'
 
 function App() {
-  return <div className='container mx-auto pt-5 max-w-2xl'></div>
+  return (
+    <div className='container m-auto'>
+      <TimeProvider>
+        <Time />
+      </TimeProvider>
+      <TableProvider>
+        <Table />
+      </TableProvider>
+    </div>
+  )
 }
 
 export default App
