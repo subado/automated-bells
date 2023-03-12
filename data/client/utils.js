9fd6d0a83f3a9456cbe37200156c23ab75f8df5e
subado <redcatgamer1980@gmail.com>
2023-03-12 22:38:42 +0400

    Delete old interface

--- data/client/utils.js
+++
@@ -1,8 +0,0 @@
-function addListeners(selector, eventType, handler, object = document) {
-	const elements = object.querySelectorAll(selector);
-	Array.from(elements).forEach((element) => {
-		element.addEventListener(eventType, handler);
-	});
-}
-
-export { addListeners };
