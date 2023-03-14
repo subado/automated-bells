fc1c75f99534db55eb9e465d2876818466db07cb
subado <redcatgamer1980@gmail.com>
2023-03-14 20:08:02 +0400

    Setup proxy '/api' for dev

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://192.168.0.177',
+    },
+  },
   plugins: [react()],
 })
