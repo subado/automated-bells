7e83bd4170bdfef0534c76b9d3cc439618f97795
subado <redcatgamer1980@gmail.com>
2023-03-16 10:21:00 +0400

    Add dropdownMenuReducer

---
+++ interface/src/reducers/dropdownMenuReducer.ts
@@ -0,0 +1,40 @@
+import type { IDropdownMenuState } from '../interfaces'
+
+export type DropdownMenuAction =
+  | {
+      type: 'push' | 'remove' | 'select'
+      item: string
+    }
+  | {
+      type: 'set'
+      list: string[]
+    }
+
+export function dropdownMenuReducer(
+  state: IDropdownMenuState,
+  action: DropdownMenuAction
+) {
+  switch (action.type) {
+    case 'push':
+      state.list.push(action.item)
+      return { ...state }
+    case 'remove':
+      return {
+        ...state,
+        list: state.list.filter((v) => {
+          if (v === action.item) {
+            return action.item
+          } else {
+            return v
+          }
+        }),
+      }
+    case 'select':
+      return {
+        ...state,
+        selected: action.item,
+      }
+    case 'set':
+      return { ...state, list: action.list }
+  }
+}
