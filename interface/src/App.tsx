e3bc75c8826aad6d394ed50caed3bb9ee0ce1132
subado <redcatgamer1980@gmail.com>
2023-03-16 22:24:13 +0400

    Add global flex

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,9 +1,14 @@
 import { Time } from './components/Time'
+import { DropdownMenu } from './components/DropdownMenu'
+import { DropdownMenuProvider } from './contexts/DropdownMenuContext'
 
 function App() {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <Time />
+      <DropdownMenuProvider>
+        <DropdownMenu name='Choose item' />
+      </DropdownMenuProvider>
     </div>
   )
 }
