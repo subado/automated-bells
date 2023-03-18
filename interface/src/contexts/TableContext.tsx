80361eef65366d4ee9a0b898bc2c88149d22abc8
subado <redcatgamer1980@gmail.com>
2023-03-18 11:20:44 +0400

    Add TableContext

---
+++ interface/src/contexts/TableContext.tsx
@@ -0,0 +1,33 @@
+import type { Dispatch } from 'react'
+import { useContext, useReducer, createContext } from 'react'
+import type { ITable, IProviderProps } from '../interfaces'
+import type { TableAction } from '../reducers/tableReducer'
+import { tableReducer } from '../reducers/tableReducer'
+
+const TableContext = createContext({} as ITable)
+const TableDispatchContext = createContext({} as Dispatch<TableAction>)
+
+export function TableProvider({ children }: IProviderProps) {
+  const [table, dispatch] = useReducer(tableReducer, initialState)
+
+  return (
+    <TableContext.Provider value={table}>
+      <TableDispatchContext.Provider value={dispatch}>
+        {children}
+      </TableDispatchContext.Provider>
+    </TableContext.Provider>
+  )
+}
+
+export function useTable() {
+  return useContext(TableContext)
+}
+
+export function useTableDispatch() {
+  return useContext(TableDispatchContext)
+}
+
+const initialState: ITable = {
+  title: '',
+  time: [],
+}
