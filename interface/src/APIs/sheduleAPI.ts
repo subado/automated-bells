502eedf09ecd7dfbd8f4a976330b9f40fd4d7a70
subado <redcatgamer1980@gmail.com>
2023-03-21 15:37:11 +0400

    Delete Title from names and rework post function

--- interface/src/APIs/sheduleAPI.ts
+++ interface/src/APIs/sheduleAPI.ts
@@ -2,12 +2,12 @@ import { api } from '../constants'
 import type { IShedule } from '../interfaces'
 
 export const sheduleAPI = {
-  async getTitle() {
+  async get() {
     const response = await api.get<IShedule>('shedule/')
     return response.data
   },
-  async postTitle(title: string) {
-    const response = await api.post<IShedule>('shedule/', { title: title })
-    return response.data
+  async post(shedule: IShedule) {
+    const response = await api.post<IShedule>('shedule/', shedule)
+    return response
   },
 }
