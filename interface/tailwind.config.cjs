817c9b30102df3b3950b9b494f58ddd0e7a44dd8
subado <redcatgamer1980@gmail.com>
2023-03-24 13:38:36 +0400

    Add animate-click

--- interface/tailwind.config.cjs
+++ interface/tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        click: {
+          '100%': { transform: 'translate(0, 2px)' },
+        },
+      },
+      animation: {
+        click: 'click 75ms',
+      },
+    },
   },
   plugins: [],
   future: {
