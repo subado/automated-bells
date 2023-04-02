0142b6262b22c1872affbda448d623c3210c5207
subado <redcatgamer1980@gmail.com>
2023-04-02 19:09:52 +0400

    Add macroses for max ssid and psk length

--- lib/framework/include/macros.h
+++ lib/framework/include/macros.h
@@ -1,5 +1,7 @@
 #pragma once
 
 #define MAX_FILENAME_LENGTH 32
+#define MAX_SSID_LENGTH     32
+#define MAX_PSK_LENGTH      64
 #define TRACE(fmt, ...)     Serial.printf(fmt, ##__VA_ARGS__)
 #define CONFIG_FILENAME     "config.json"
