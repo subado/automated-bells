db68d2799802b53acfca27aa11cc7ececac3abdf
subado <redcatgamer1980@gmail.com>
2023-04-09 21:54:06 +0400

    Add new interfaces for '/ntp', '/wifi' and rename 'ITable' -> 'ITableTime'

--- interface/src/interfaces.ts
+++ interface/src/interfaces.ts
@@ -1,6 +1,11 @@
-export interface ITable {
+export interface ITableTime {
   title: string
   time: string[]
+}
+
+export interface ITable {
+  title: string
+  items: string[]
 }
 
 export interface ITableTitles {
@@ -19,4 +24,19 @@ export interface IChildrenProps {
 
 export interface IScheduler {
   title: string
+}
+
+export interface INtp {
+  servers: string[]
+  timeZone: number
+}
+
+export interface IWiFiDetails {
+  ssid: string
+  psk: string
+}
+
+export interface IWiFi {
+  sta: IWiFiDetails
+  ap: IWiFiDetails
 }
