f1cc6d406d062cda9f5639d95db440ee4526e33b
subado <redcatgamer1980@gmail.com>
2023-04-10 16:55:09 +0400

    Add Form

---
+++ interface/src/components/Form.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import type { IChildrenProps } from '../interfaces'
+import { Button } from './Button'
+import { ButtonList } from './ButtonList'
+
+interface IFormProps extends IChildrenProps {
+  onSubmit: React.FormEventHandler<HTMLFormElement>
+  onClear: React.MouseEventHandler<HTMLButtonElement>
+  submitButtonContent: string
+  clearButtonContent: string
+}
+
+export function Form({
+  children,
+  onSubmit,
+  onClear,
+  submitButtonContent,
+  clearButtonContent,
+}: IFormProps) {
+  return (
+    <form
+      className='flex flex-col items-center gap-y-3 px-2'
+      onSubmit={onSubmit}
+    >
+      {children}
+      <ButtonList buttonClassName='p-2'>
+        <Button color='orange' type='submit'>
+          {submitButtonContent}
+        </Button>
+        <Button color='sky' onClick={onClear}>
+          {clearButtonContent}
+        </Button>
+      </ButtonList>
+    </form>
+  )
+}
