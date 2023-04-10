36f4bcc056ef8ed2ccba3ae4300272e40470fdfb
subado <redcatgamer1980@gmail.com>
2023-04-10 21:52:06 +0400

    And restart button to menu

--- interface/src/components/Menu/Menu.tsx
+++ interface/src/components/Menu/Menu.tsx
@@ -1,11 +1,25 @@
+import { api } from '../../constants'
+import { Button } from '../Button'
 import { MenuItem } from './MenuItem'
 
 export function Menu() {
+  function handleRestart() {
+    api.post('restart/')
+  }
+
   return (
-    <ul className='flex flex-col gap-y-6 absolute h-screen w-1/3 z-10 py-8 px-3 bg-white shadow-xl shadow-black text-center text-2xl'>
+    <ul className='absolute h-screen w-1/3 z-10 py-8 px-3 bg-white shadow-xl shadow-black text-center text-2xl flex flex-col gap-y-6'>
       <MenuItem to='/'>Time</MenuItem>
       <MenuItem to='/ntp/'>NTP</MenuItem>
       <MenuItem to='/wifi/'>WiFi</MenuItem>
+      <span className='border' />
+      <Button
+        className='px-1 py-2 font-normal rounded-none'
+        color='pink'
+        onClick={handleRestart}
+      >
+        Restart
+      </Button>
     </ul>
   )
 }
