5bc42081de0a5ccb127313f611bac92c99bd1d0a
subado <redcatgamer1980@gmail.com>
2023-04-10 19:19:24 +0400

    Text sizes and styling

--- interface/src/components/Header/Header.tsx
+++ interface/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const [, setMenuOpen] = useMenuOpen()
 
   return (
-    <header className='inline-flex justify-between w-full py-1 px-2 bg-gray-100 shadow-sm shadow-gray-500 rounded-b sticky top-0'>
+    <header className='inline-flex justify-between w-full mb-2 p-1 bg-gray-100 shadow-sm shadow-gray-500 rounded-b sticky top-0 text-2xl md:text-3xl'>
       <Button
         color='lightGray'
         className='w-[15%] pr-2'
