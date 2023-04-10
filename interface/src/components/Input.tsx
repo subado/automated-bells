8b12a29662042eda0a6980821ad1084380a0f92a
subado <redcatgamer1980@gmail.com>
2023-04-10 19:18:19 +0400

    Rounded and change styles

--- interface/src/components/Input.tsx
+++ interface/src/components/Input.tsx
@@ -8,7 +8,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function Input({ label, id, ...rest }: InputProps) {
   rest = {
     ...rest,
-    className: `p-2 tracking-wide placeholder:italic placeholder:text-slate-400 w-full border-2 bg-gray-100 font-bold text-center ${
+    className: `p-2 rounded tracking-wide placeholder:italic placeholder:text-slate-400 w-full border-2 bg-gray-100 font-semibold text-center ${
       rest.className || ''
     }`,
     placeholder: label,
@@ -16,8 +16,8 @@ export function Input({ label, id, ...rest }: InputProps) {
   }
 
   return (
-    <div className='inline-flex flex-row gap-x-2 items-center w-full'>
-      <label className='font-semibold text-center w-1/5' htmlFor={id}>
+    <div className='flex flex-col gap-y-0 w-full'>
+      <label className='text-slate-600 text-[115%] text-left' htmlFor={id}>
         {label}
       </label>
       <input id={id} {...rest} />
