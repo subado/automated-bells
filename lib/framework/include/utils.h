019403e29c9898d674a8fbaaaf90e11f94343df4
subado <redcatgamer1980@gmail.com>
2023-04-11 18:05:49 +0400

    Add getFileName() and rework removeExtension()

--- lib/framework/include/utils.h
+++ lib/framework/include/utils.h
@@ -3,5 +3,6 @@
 namespace utils
 {
 void getPathToTable(char *dest, const char *title);
+void getFileName(char *dest, const char *path, size_t size);
 void removeExtension(char *fileName);
 } // namespace utils
--- lib/framework/src/utils.cpp
+++ lib/framework/src/utils.cpp
@@ -5,12 +5,34 @@
 
 void utils::getPathToTable(char *dest, const char *title)
 {
-  char path[MAX_FILENAME_LENGTH]{};
-  std::snprintf(path, sizeof(path), "/tables/%s%s", title, ".json");
-  std::strncpy(dest, path, MAX_FILENAME_LENGTH);
+  std::snprintf(dest, MAX_FILENAME_LENGTH, "/tables/%s%s", title, ".json");
+}
+
+void utils::getFileName(char *dest, const char *path, size_t size)
+{
+  char buf[MAX_FILENAME_LENGTH]{};
+  std::strncpy(buf, path, sizeof(buf));
+  for (char *start = buf, *end = buf; *start != '\0'; ++end)
+  {
+    if (*end == '\0')
+    {
+      for (size_t i = 0; i < size && start <= end; ++i, ++start)
+      {
+        dest[i] = *start;
+      }
+    }
+    else if (*end == '/')
+    {
+      start = ++end;
+    }
+  }
 }
 
 void utils::removeExtension(char *fileName)
 {
-  *(std::strchr(fileName, '.')) = '\0';
+  char *extension = std::strchr(fileName, '.');
+  while (extension != nullptr && *extension != '\0')
+  {
+    *(extension++) = '\0';
+  }
 }
