b515027fba423db1616812320b17da3d8a27f2ea
subado <redcatgamer1980@gmail.com>
2023-04-11 20:03:37 +0400

    Fix max psk length

--- lib/framework/include/macros.h
+++ lib/framework/include/macros.h
@@ -2,6 +2,6 @@
 
 #define MAX_FILENAME_LENGTH 32
 #define MAX_SSID_LENGTH     32
-#define MAX_PSK_LENGTH      64
+#define MAX_PSK_LENGTH      65
 #define TRACE(fmt, ...)     Serial.printf(fmt, ##__VA_ARGS__)
 #define CONFIG_FILENAME     "config.json"
